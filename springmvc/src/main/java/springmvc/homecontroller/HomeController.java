552d87b376e9a5f34239894f3df83d309a2cca08
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-01 11:36:35 +0530

    model class used to retrieve value from the controller

--- springmvc/src/main/java/springmvc/homecontroller/HomeController.java
+++ springmvc/src/main/java/springmvc/homecontroller/HomeController.java
@@ -1,14 +1,31 @@
 package springmvc.homecontroller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("this is home url...");
+		
+		model.addAttribute("name", "Awanish kumar singh");
+		
+		model.addAttribute("id", 12);
+		
+		List<String> friends = new ArrayList<String>();
+		
+		friends.add("manish");
+		friends.add("sunish");
+		friends.add("bhawna");
+		
+		model.addAttribute("friends",friends);
+		
 		return "index";
 	}
 	
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -11,7 +12,27 @@
 	<h1>Controller</h1>
 	<h1>This is my home page.</h1>
 	<h1>This is the new page created by the page.</h1>
-	<a href = "about">About</a>
-	
+	<a href="about">About</a>
+
+	<%
+		String name = (String) request.getAttribute("name");
+		int id = (Integer)request.getAttribute("id");
+	%>
+
+	<h1><%=name%></h1>
+
+	<h1>
+		Id =
+		<%= id %></h1>
+
+<%
+List<String> list = (List<String>)request.getAttribute("friends");
+for(String s : list){
+%>
+
+<h1><%=  s %></h1>
+
+<%} %>
+
 </body>
 </html>
