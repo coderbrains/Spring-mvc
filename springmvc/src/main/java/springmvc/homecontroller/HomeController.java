56bb208e9aeb26270bb75938bff6e99adb9b4054
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-01 12:14:35 +0530

    model and view is implemented

--- springmvc/src/main/java/springmvc/homecontroller/HomeController.java
+++ springmvc/src/main/java/springmvc/homecontroller/HomeController.java
@@ -1,11 +1,14 @@
 package springmvc.homecontroller;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -33,4 +36,22 @@ public class HomeController {
 	public String about() {
 		return "about";
 	}
+	
+	@RequestMapping("/help")
+	public ModelAndView help() {
+		
+		ModelAndView view = new ModelAndView();
+		
+		view.setViewName("help");
+		view.addObject("name", "Awanish");
+		view.addObject("roll", 1232);
+		LocalDateTime time = LocalDateTime.now();
+		view.addObject("time",time);
+		return view;
+		
+	}
+	
+	
+	
+	
 }
