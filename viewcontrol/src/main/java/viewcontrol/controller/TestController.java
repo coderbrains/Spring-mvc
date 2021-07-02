a2cf67c4c57ddf137fa6c018cc8055d921de56b0
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-03 00:11:43 +0530

    successfully added

--- viewcontrol/src/main/java/viewcontrol/controller/TestController.java
+++ viewcontrol/src/main/java/viewcontrol/controller/TestController.java
@@ -5,32 +5,51 @@ import java.net.http.HttpRequest;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
 
 	@RequestMapping("/")
 	public String home() {
-		
+
 		return "index";
-		
+
 	}
-	
-	@RequestMapping(path = "processform" , method = RequestMethod.POST)
-	public String process(HttpServletRequest request) {
-		System.out.println("hello this is awanish kumar singh");
+
+	@RequestMapping(path = "processform", method = RequestMethod.POST)
+	public String process(HttpServletRequest request,
+
+			@RequestParam("email") String email, @RequestParam("name") String name, @RequestParam("pass") String pass,
+			Model model
+
+	) {
 		
-		String name = request.getParameter("name");
-		String email = request.getParameter("email");
-		String pass = request.getParameter("pass");
+		model.addAttribute("name", name);
+		model.addAttribute("email", email);
+		model.addAttribute("password", pass);
+		
+		
+		System.out.println("hello this is awanish kumar singh");
+
+		String name1 = request.getParameter("name");
+		String email1 = request.getParameter("email");
+		String pass1 = request.getParameter("pass");
+
+		System.out.println(name1);
+		System.out.println(email1);
+		System.out.println(pass1);
+		System.out.println();
+		System.out.println();
 		
 		System.out.println(name);
 		System.out.println(email);
 		System.out.println(pass);
-		
+
 		return "submit";
 	}
-	
+
 }
--- viewcontrol/src/main/webapp/WEB-INF/views/submit.jsp
+++ viewcontrol/src/main/webapp/WEB-INF/views/submit.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    
+    <%@page isELIgnored="false" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +10,9 @@
 </head>
 <body>
 <h1>Form submitted successfully</h1>
+
+<h1>name is ${ name } </h1>
+<h1>email is ${email }</h1>
+<h1>password is ${password }</h1>
 </body>
 </html>
