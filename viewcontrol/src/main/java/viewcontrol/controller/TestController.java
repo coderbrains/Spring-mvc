c3bc86ab74b20dbb4027d797ee32581e2565ede1
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-02 23:25:55 +0530

    all the params have been added

---
+++ viewcontrol/src/main/java/viewcontrol/controller/TestController.java
@@ -0,0 +1,21 @@
+package viewcontrol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/")
+	public String home() {
+		
+		return "index";
+		
+	}
+	
+	@RequestMapping("/submit")
+	public String hom() {
+		return "submit";
+	}
+	
+}
